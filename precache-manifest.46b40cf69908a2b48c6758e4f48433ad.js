ee9416e3748e15f05d6b985a91f7a0d4d6a1ce50
klonek105 <klonek105@gmail.com>
2021-03-23 12:23:10 +0100

    Updates

---
+++ precache-manifest.46b40cf69908a2b48c6758e4f48433ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc5960088af492ca1709",
+    "url": "/app1/static/css/main.6dbc4ccd.chunk.css"
+  },
+  {
+    "revision": "bc5960088af492ca1709",
+    "url": "/app1/static/js/main.c1e84099.chunk.js"
+  },
+  {
+    "revision": "9c5b48e09cfe3e7e0349",
+    "url": "/app1/static/js/runtime~main.806c9977.js"
+  },
+  {
+    "revision": "4c59afacec54cb918c28",
+    "url": "/app1/static/js/2.da5fdfa2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21c8755eca5238793cc7c639bb8c6be6",
+    "url": "/app1/index.html"
+  }
+];
